b5653e6d26d1ee6174da58fe23db68d1aef7b247
Narendra Kumar <nkumar@collab.net>
2022-04-09 22:59:19 +0200

    Add some more utility functions

--- functions.go
+++ functions.go
@@ -4,21 +4,61 @@ import (
 	"encoding/base64"
 	"filippo.io/age"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
-func Base64Encode(input string) {
-	encoded := base64.StdEncoding.EncodeToString([]byte(input))
-	fmt.Printf("Base64 encoding of '%s' is '%s'", input, encoded)
+type KeyPair struct {
+	Public  string
+	Private string
 }
 
-func GenerateX25519Identity() {
+func Base64Encode(input string) string {
+	encoded := base64.StdEncoding.EncodeToString([]byte(input))
+	return encoded
+}
+
+func Base64Decode(input string) (string, error) {
+	decoded, err := base64.StdEncoding.DecodeString(input)
+	if err != nil {
+		return "", err
+	}
+	return string(decoded), nil
+}
+
+func ParsePassword(input string) (string, error) {
+	str, err := Base64Decode(input)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse password. caused by '%v'", err)
+	}
+	output := Reverse(str)
+	return output, nil
+}
+
+func ValidatePath(path *string, flag string) (string, error) {
+	trimmed := strings.TrimSpace(*path)
+	if len(trimmed) == 0 {
+		log.Fatalf("flag '%s' is required", flag)
+	}
+	decoded, err := Base64Decode(trimmed)
+	if err != nil {
+		return "", fmt.Errorf("failed to validate path. error in decoding. error: '%v'", err)
+	}
+	if _, err := os.Stat(decoded); os.IsNotExist(err) {
+		log.Fatalf("invalid '%s'. Path '%s' does not exist.", flag, decoded)
+	}
+	return decoded, nil
+}
+
+func GenerateX25519Identity() KeyPair {
 	id, _ := age.GenerateX25519Identity()
 	keyPri := id.String()
 	keyPub := id.Recipient().String()
-	fmt.Println("------- Generated X25519Identity START --------")
-	fmt.Printf("Private Key (bas64 encoded): '%s'\n", base64.StdEncoding.EncodeToString([]byte(keyPri)))
-	fmt.Printf("Public Key (bas64 encoded): '%s'\n", base64.StdEncoding.EncodeToString([]byte(keyPub)))
-	fmt.Println("------- Generated X25519Identity END --------")
+	return KeyPair{
+		Public:  keyPub,
+		Private: keyPri,
+	}
 }
 
 func Reverse(str string) string {
